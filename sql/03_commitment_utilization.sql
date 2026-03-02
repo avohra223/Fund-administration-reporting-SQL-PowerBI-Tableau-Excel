f31f783009808d1cbb779283852279c9e96faccd
avohra223 <akhil.vohra1897@gmail.com>
2026-03-02 09:54:19 +0100

    Update 03_commitment_utilization.sql

--- sql/03_commitment_utilization.sql
+++ sql/03_commitment_utilization.sql
@@ -1,16 +1,17 @@
 -- Query 3: Commitment Utilization by LP
 -- Business question: What percentage of each LP's commitment has been called so far?
 
+WITH Committed AS (
+    SELECT Investor_ID, SUM(Commitment_Amount) AS Total_Committed
+    FROM Commitments
+    GROUP BY Investor_ID
+),
+Called AS (
+    SELECT Investor_ID, SUM(Amount_Fund_Base) AS Total_Called
+    FROM Transactions
+    WHERE Txn_Type = 'Capital Call'
+    GROUP BY Investor_ID
+)
 SELECT
     Investors.Investor_Name,
-    SUM(Commitments.Commitment_Amount) AS Total_Committed,
-    SUM(CASE WHEN Transactions.Txn_Type = 'Capital Call' 
-        THEN Transactions.Amount_Fund_Base ELSE 0 END) AS Total_Called,
-    ROUND(SUM(CASE WHEN Transactions.Txn_Type = 'Capital Call' 
-        THEN Transactions.Amount_Fund_Base ELSE 0 END) * 100.0 / 
-        SUM(Commitments.Commitment_Amount), 2) AS Utilization_Pct
-FROM Investors
-JOIN Commitments ON Investors.Investor_ID = Commitments.Investor_ID
-JOIN Transactions ON Investors.Investor_ID = Transactions.Investor_ID
-GROUP BY Investors.Investor_Name
-ORDER BY Utilization_Pct DESC;
+    Committed.Total_Committ
